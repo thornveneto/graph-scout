6554eae9ee8ed1e4c7bc53b3217895f09c63fa62
M K <thorncmr@gmail.com>
2021-08-22 00:08:53 +1000

    exporting additional types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,3 @@
 export { GraphHelper } from "./GraphHelper";
-export { GraphDataAccess, BFSSearchConfig, SearchConfigFilterFunction, _adjacentListMapRecordItem, _ADJACENT_VERTEX, _VISITED_ITEM } from "./types";
+export { GraphDataAccess, BFSSearchConfig, SearchConfigFilterFunction, _adjacentListMapRecordItem, _ADJACENT_VERTEX, _VISITED_ITEM, _adjacentListMapRecord, _adjacentListMap, _llFromVertexId, _llToVertexId, _llLinkId, } from "./types";
 //# sourceMappingURL=index.d.ts.map
